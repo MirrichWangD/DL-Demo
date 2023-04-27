59d024b8a81080d3d0c04952462c89073ea94c52
MirrichWangD <2693970161@qq.com>
2023-04-27 15:32:24 +0800

    提交代码

--- Machine-Translation/Transformer/Tatoeba-Translation/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-",
-"""
-    @File        : __init__.py.py,
-    @Time        : 2022/10/16 15:46
-    @Author      : Mirrich Wang ",
-    @Version     : Python 3.x.x (env)",
-    @Description : None"
-"""
