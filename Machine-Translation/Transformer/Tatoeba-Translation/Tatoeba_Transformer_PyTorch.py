9a4783ad301d2fae9c28e754358e81e2770ef5e1
MirrichWangD <2693970161@qq.com>
2023-04-27 09:45:16 +0800

    修改头部注释，删除函数的device传参

--- Machine-Translation/Transformer/Tatoeba-Translation/Tatoeba_Transformer_PyTorch.py
+++ Machine-Translation/Transformer/Tatoeba-Translation/Tatoeba_Transformer_PyTorch.py
@@ -6,36 +6,21 @@
 @ Author      : Mirrich Wang
 @ Version     : Python 3.8.12 (Conda)
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-数据集：https://www.manythings.org/anki/ Mandarin Chinese - English (cmn-eng.zip)
-模型参考：https://pytorch.org/tutorials/beginner/translation_transformer.html?highlight=transformer%20translation
+数据集：https://www.manythings.org/anki/
+模型参考文档：https://pytorch.org/tutorials/beginner/translation_transformer.html?highlight=transformer%20translation
+简要概述：
+    暂时只实验了中英翻译，cmn-eng/cmn.txt 繁中转换成了简中（参考 utils/cmn-zh.py）。该代码采用 Spacy 3.5 语言模型作为句子分词器，因此需
+    要提前安装对应语言模块，如 英文-简中 翻译，需要安装 zh_core_web_sm 和 en_core_web_sm，这些均可以在下方 SPACY 变量进行调整
 实验设备：
-- 10th Gen Intel(R) Core(TM) i5-10400
-- Nvidia GeForce RTX2060-Super 8GB
-实验环境：
-- Torch 1.12.1+cu116
-- Torchtext 0.13.1
-- Spacy 3.5.0
-实验终端日志 (cmn-eng)：
-Load Data: 100%|██████████| 29155/29155 [00:10<00:00, 2822.88it/s]
-================== Dataset Info ==================
-| size                  | 29155                  |
-| src vocab: en         | 7899                   |
-| tgt vocab: zh         | 15242                  |
-==================================================
-Start training...
-Epoch: 1/100: 100%|██████████| 439/439 [00:53<00:00,  8.18it/s, loss=4.7350, acc=32.80%, loss_val=4.4263, acc_val=33.02%]
-Epoch: 2/100: 100%|██████████| 439/439 [00:52<00:00,  8.41it/s, loss=4.2578, acc=40.20%, loss_val=3.8171, acc_val=40.31%]
-Epoch: 3/100: 100%|██████████| 439/439 [00:51<00:00,  8.46it/s, loss=3.7440, acc=43.37%, loss_val=3.4022, acc_val=43.49%]
-Epoch: 4/100: 100%|██████████| 439/439 [00:51<00:00,  8.47it/s, loss=3.3441, acc=45.86%, loss_val=3.0875, acc_val=45.99%]
-Epoch: 5/100: 100%|██████████| 439/439 [00:52<00:00,  8.43it/s, loss=3.1428, acc=48.14%, loss_val=2.9378, acc_val=48.29%]
-...
-Epoch: 95/100: 100%|██████████| 439/439 [00:51<00:00,  8.59it/s, loss=0.0097, acc=98.59%, loss_val=0.0537, acc_val=98.61%]
-Epoch: 96/100: 100%|██████████| 439/439 [00:51<00:00,  8.59it/s, loss=0.0220, acc=98.60%, loss_val=0.0211, acc_val=98.63%]
-Epoch: 97/100: 100%|██████████| 439/439 [00:51<00:00,  8.60it/s, loss=0.0149, acc=98.65%, loss_val=0.0312, acc_val=98.67%]
-Epoch: 98/100: 100%|██████████| 439/439 [00:51<00:00,  8.56it/s, loss=0.0326, acc=98.65%, loss_val=0.0228, acc_val=98.68%]
-Epoch: 99/100: 100%|██████████| 439/439 [00:51<00:00,  8.56it/s, loss=0.0482, acc=98.70%, loss_val=0.0214, acc_val=98.72%]
-Epoch: 100/100: 100%|██████████| 439/439 [00:51<00:00,  8.59it/s, loss=0.0211, acc=98.65%, loss_val=0.0249, acc_val=98.67%]
-Time taken: 5146s
+    CPU: 10th Gen Intel(R) Core(TM) i5-10400
+    GPU:  Nvidia GeForce RTX2060-Super 8GB
+依赖模块（需要自行安装，没有版本号默认最新）：
+    torch 1.12.1+cu116
+    torchtext 0.13.1
+    spacy 3.5.0
+    matplotlib
+    pandas
+    tqdm
 +++++++++++++++++++++++++++++++++++
 """
 
@@ -60,7 +45,6 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset, DataLoader, BatchSampler
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.data import get_tokenizer
-from torchtext.data.metrics import bleu_score
 
 # 设置忽略warning信息
 warnings.filterwarnings("ignore")
@@ -184,7 +168,7 @@ class TranslationDataset(Dataset):
 ++++++++++++++++++++++"""
 
 
-def generate_square_subsequent_mask(sz: int, device: torch.device) -> Tensor:
+def generate_square_subsequent_mask(sz: int) -> Tensor:
     """
     生成方形后续掩码
     Args:
@@ -195,19 +179,18 @@ def generate_square_subsequent_mask(sz: int, device: torch.device) -> Tensor:
         torch.tensor
     """
     # 生成倒三角全为 0 的矩阵
-    mask = (torch.triu(torch.ones((sz, sz), device=device)) == 1).transpose(0, 1)
+    mask = (torch.triu(torch.ones((sz, sz))) == 1).transpose(0, 1)
     # 将上三角全部使用 -inf 填充（不包括对角线）
     mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
     return mask
 
 
-def create_mask(src: Tensor, tgt: Tensor, device: torch.device) -> object:
+def create_mask(src: Tensor, tgt: Tensor) -> object:
     """
     根据 src、tgt 生成 src_mask、tgt_mask、src_padding_mask、tgt_padding_mask
     Args:
         src: torch.Tensor 输入词索引向量 (S_src, N)
         tgt: torch.Tensor 输出词索引向量 (S_tgt, N)
-        device: torch.device 运算硬件
 
     Returns:
         Tensor(S_src, S_src), Tensor(S_tgt, S_tgt), Tensor(N, S_src), Tensor(N, S_tgt)
@@ -216,8 +199,8 @@ def create_mask(src: Tensor, tgt: Tensor, device: torch.device) -> object:
     tgt_seq_len = tgt.shape[0]
 
     # 生成方阵掩码
-    tgt_mask = generate_square_subsequent_mask(tgt_seq_len, device)
-    src_mask = torch.zeros((src_seq_len, src_seq_len), device=device).type(torch.bool)
+    tgt_mask = generate_square_subsequent_mask(tgt_seq_len).to(src.device)
+    src_mask = torch.zeros((src_seq_len, src_seq_len), device=src.device).type(torch.bool)
 
     # 生成词索引 padding 的掩码
     src_padding_mask = (src == PAD_IDX).transpose(0, 1)
@@ -343,8 +326,7 @@ def greedy_decode(model: nn.Module,
                   src: Tensor,
                   src_mask: Tensor,
                   max_len: int,
-                  start_symbol: int,
-                  device: torch.device) -> Tensor:
+                  start_symbol: int) -> Tensor:
     """
     预测所使用的贪婪解码算法
     Args:
@@ -358,14 +340,10 @@ def greedy_decode(model: nn.Module,
     Returns:
         Tensor(S_pred, 1)
     """
-    src = src.to(device)
-    src_mask = src_mask.to(device)
-
     memory = model.encode(src, src_mask)
-    ys = torch.ones(1, 1).fill_(start_symbol).type(torch.long).to(device)
+    ys = torch.ones(1, 1).fill_(start_symbol).type(torch.long).to(src.device)
     for i in range(max_len - 1):
-        memory = memory.to(device)
-        tgt_mask = (generate_square_subsequent_mask(ys.size(0), device).type(torch.bool)).to(device)
+        tgt_mask = (generate_square_subsequent_mask(ys.size(0)).type(torch.bool)).to(src.device)
         out = model.decode(ys, memory, tgt_mask)
         out = out.transpose(0, 1)
         prob = model.generator(out[:, -1])
@@ -418,6 +396,10 @@ if __name__ == "__main__":
     # 将输入和翻译语言构造索引
     LANGUAGE[args.src_lang] = 0
     LANGUAGE[args.tgt_lang] = 1
+    if args.src_lang not in SPACY.keys() or args.tgt_lang not in SPACY.keys():
+        raise "请确保 spacy 模块中是否包含设置的输入、输出语言！"
+    # 设置开始轮数
+    start_epoch = -1
     # 设置训练硬件
     args.device = torch.device(args.device) if torch.cuda.is_available() else torch.device("cpu")
     # 获取输出目录
@@ -461,24 +443,30 @@ if __name__ == "__main__":
                         len(dataset.tgt_vocab),
                         args.d_ff,
                         args.dropout).to(args.device)
-    # 导入模型权重
-    if args.resume:
-        model.load_state_dict(torch.load(args.resume))
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.98), eps=1e-9)
     criterion = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
+    # 设置断点训练
+    if args.resume:
+        resume = torch.load(args.resume)
+        start_epoch = resume["epoch"]
+        model.load_state_dict(resume["model"])
+        optimizer.load_state_dict(resume["optimizer"])
 
     # ---------------------------- #
     # Training and Evaluate Model
     # ---------------------------- #
 
     print("Start training...")
+    BEST_ACC = 0.  # 记录最优准确率的 epoch
+    # 记录每个 step 的损失喝准确率
     LOSS = {"train": [[] for _ in range(args.epochs)], "val": [[] for _ in range(args.epochs)]}
     ACC = {"train": [[] for _ in range(args.epochs)], "val": [[] for _ in range(args.epochs)]}
+
     st = time.time()
-    for epoch in range(args.epochs):
+    for epoch in range(start_epoch + 1, args.epochs):
         # 初始化准确率计算变量
-        correct, total = 0, 0
+        correct, total = {"train": 0, "val": 0}, {"train": 0, "val": 0}
 
         model.train()
         with tqdm(total=len(train_db), desc=f"Epoch: {epoch + 1}/{args.epochs}") as pbar:  # 训练进度条
@@ -488,7 +476,7 @@ if __name__ == "__main__":
                 tgt_input = tgt[:-1, :]  # 前移一位，不需要终止符
 
                 # 生成 mask
-                src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input, args.device)
+                src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input)
 
                 # 计算每个词的概率
                 logits = model(src, tgt_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
@@ -503,20 +491,25 @@ if __name__ == "__main__":
                 optimizer.step()  # 更新模型参数
 
                 # 计算准确率
-                pred = torch.argmax(logits, -1)
-                for i in range(pred.shape[1]):
-                    L = (tgt_out[:, i] != PAD_IDX).sum()
-                    total += L.item()
-                    correct += (pred[:L, i] == tgt_out[:L, i]).sum().item()
+                pred = torch.argmax(F.log_softmax(logits, -1), -1)
+                L = (tgt_out != PAD_IDX).sum(0)
+                total["train"] += L.sum().item()
+                correct["train"] += sum(map(
+                    lambda i: (pred[:L[i], i] == tgt_out[:L[i], i]).sum().item(), range(pred.shape[1])
+                ))
+                # # 计算准确率
+                # for i in range(pred.shape[1]):
+                #     L = (tgt_out[:, i] != PAD_IDX).sum()
+                #     total["train"] += L.item()
+                #     correct["train"] += (pred[:L, i] == tgt_out[:L, i]).sum().item()
 
                 # 记录损失、准确率
                 LOSS["train"][epoch].append(loss.item())
-                ACC["train"][epoch].append(correct / total)
+                ACC["train"][epoch].append(correct["train"] / total["train"])
                 # 更新进度条
-                pbar_dict = {"loss": f"{loss.item():.4f}",
-                             "acc": f"{(correct / total) * 100:.2f}%"}
                 pbar.update(1)
-                pbar.set_postfix(pbar_dict)
+                pbar.set_postfix({"loss": f"{np.mean(LOSS['train'][epoch]).item():.4f}",
+                                  "acc": f"{np.mean(ACC['train'][epoch]).item() * 100:.2f}%"})
 
             # 训练完一个 epoch 后进行验证
             model.eval()
@@ -524,32 +517,42 @@ if __name__ == "__main__":
                 src, tgt = src.to(args.device), tgt.to(args.device)
                 tgt_input = tgt[:-1, :]
 
-                src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input, args.device)
+                src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input)
 
                 logits = model(src, tgt_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
 
                 tgt_out = tgt[1:, :]
                 loss = criterion(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1))
 
-                pred = torch.argmax(logits, -1)
-                for i in range(pred.shape[1]):
-                    L = (tgt_out[:, i] != PAD_IDX).sum()
-                    total += L.item()
-                    correct += (pred[:L, i] == tgt_out[:L, i]).sum().item()
+                # 计算准确率
+                pred = torch.argmax(F.log_softmax(logits, -1), -1)
+                L = (tgt_out != PAD_IDX).sum(0)
+                total["val"] += L.sum().item()
+                correct["val"] += sum(map(
+                    lambda i: (pred[:L[i], i] == tgt_out[:L[i], i]).sum().item(), range(pred.shape[1])
+                ))
 
                 # 记录验证损失、准确率
                 LOSS["val"][epoch].append(loss.item())
-                ACC["val"][epoch].append(correct / total)
+                ACC["val"][epoch].append(correct["val"] / total["val"])
                 # 更新进度条
-                pbar_dict.update({"loss_val": f"{loss.item():.4f}",
-                                  "acc_val": f"{(correct / total) * 100:.2f}%"})
-                pbar.set_postfix(pbar_dict)
+                pbar.set_postfix({"loss": f"{np.mean(LOSS['train'][epoch]).item():.4f}",
+                                  "acc": f"{np.mean(ACC['train'][epoch]).item() * 100:.2f}%",
+                                  "loss_val": f"{np.mean(LOSS['val'][epoch]).item():.4f}",
+                                  "acc_val": f"{np.mean(ACC['val'][epoch]).item() * 100:.2f}%"})
+
+            # 判断验证数据平均准确率是否大于最优准确率，若更高则保存 best_model.pth
+            if np.mean(ACC["val"][epoch]).item() > BEST_ACC:
+                BEST_ACC = np.mean(ACC["val"][epoch]).item()
+                torch.save({"model": model.state_dict(), "optimizer": optimizer.state_dict(), "epoch": epoch},
+                           output_dir / "best_model.pth")
 
     et = time.time()
     print("Time taken: %ds" % (et - st))
 
     # 保存最终的模型权重
-    torch.save(model.state_dict(), output_dir / "model.pth")
+    torch.save({"model": model.state_dict(), "optimizer": optimizer.state_dict(), "epoch": args.epochs},
+               output_dir / "final_model.pth")
     print("模型权重保存到：%s" % output_dir)
     # 保存损失、准确率
     print("训练、验证损失和准确率保存到：%s" % (output_dir / "metrics.json"))
@@ -560,16 +563,10 @@ if __name__ == "__main__":
     # Visualization
     # ------------------------ #
 
-    # 获取每个 epoch 的平均值
-    train_loss = np.mean(LOSS["train"], axis=1)
-    train_acc = np.mean(ACC["train"], axis=1)
-    val_loss = np.mean(LOSS["val"], axis=1)
-    val_acc = np.mean(ACC["val"], axis=1)
-
     # 绘制损失图
     plt.figure()
-    plt.plot(train_loss, label="Train")
-    plt.plot(val_loss, label="Val")
+    plt.plot(np.mean(LOSS["train"], 1), label="Train")
+    plt.plot(np.mean(LOSS["val"], 1), label="Val")
     plt.legend(loc="best")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
@@ -577,8 +574,8 @@ if __name__ == "__main__":
     plt.savefig(output_dir / "loss.png")
     # 绘制精准率图
     plt.figure()
-    plt.plot(train_acc, label="Train")
-    plt.plot(val_acc, label="Val")
+    plt.plot(np.mean(ACC["train"], 1), label="Train")
+    plt.plot(np.mean(ACC["val"], 1), label="Val")
     plt.legend(loc="best")
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
@@ -599,18 +596,20 @@ if __name__ == "__main__":
             src, tgt = src.to(args.device), tgt.to(args.device)
             tgt_input = tgt[:-1, :]
 
-            src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input, args.device)
+            src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input)
 
             logits = model(src, tgt_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
 
             tgt_out = tgt[1:, :]
             loss = criterion(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1))
 
-            pred = torch.argmax(logits, -1)
-            for i in range(pred.shape[1]):
-                L = (tgt_out[:, i] != PAD_IDX).sum()
-                total += L.item()
-                correct += (pred[:L, i] == tgt_out[:L, i]).sum().item()
+            # 计算准确率
+            pred = torch.argmax(F.log_softmax(logits, -1), -1)
+            L = (tgt_out != PAD_IDX).sum(0)
+            total += L.sum().item()
+            correct += sum(map(
+                lambda i: (pred[:L[i], i] == tgt_out[:L[i], i]).sum(), range(pred.shape[1])
+            ))
             pbar.update(1)
             pbar.set_postfix({"loss_val": f"{loss.item():.4f}",
                               "acc_val": f"{(correct / total) * 100:.2f}%"})
@@ -623,8 +622,8 @@ if __name__ == "__main__":
     for src, tgt in test_db:
         src = src.to(args.device)
         num_tokens = src.shape[0]
-        src_mask = (torch.zeros(num_tokens, num_tokens)).type(torch.bool)
-        tgt_tokens = greedy_decode(model, src, src_mask, num_tokens + 5, BOS_IDX, args.device)
+        src_mask = (torch.zeros(num_tokens, num_tokens)).type(torch.bool).to(args.device)
+        tgt_tokens = greedy_decode(model, src, src_mask, num_tokens + 5, BOS_IDX)
 
         print("原始翻译：", " ".join(dataset.tgt_vocab.lookup_tokens(tgt.flatten().tolist())))
         print("模型翻译：", " ".join(dataset.tgt_vocab.lookup_tokens(tgt_tokens.flatten().tolist())))
