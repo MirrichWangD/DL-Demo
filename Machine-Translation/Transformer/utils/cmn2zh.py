6371e15818a21c950f9b5fa3156623468ae5dfa1
Mirrich:D <2693970161@qq.com>
2024-04-09 21:41:17 +0800

    更换data路径

--- Machine-Translation/Transformer/utils/cmn2zh.py
+++ Machine-Translation/Transformer/utils/cmn2zh.py
@@ -14,6 +14,6 @@ import opencc
 import pandas as pd
 
 cc = opencc.OpenCC("t2s")
-data = pd.read_table("./data/eng-cmn.txt", header=None)
+data = pd.read_table("../Tab-Separator/data/eng-cmn.txt", header=None)
 data[1] = data[1].apply(lambda i: cc.convert(i))
-data.to_csv("./data/eng-zh.txt", header=None, index=False, sep="\t")
+data.to_csv("../Tab-Separator/data/eng-zh.txt", header=None, index=False, sep="\t")
