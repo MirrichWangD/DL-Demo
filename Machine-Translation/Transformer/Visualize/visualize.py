a39aab4b5d10847f224c881cbcd93b4684ec02a9
Mirrich:D <2693970161@qq.com>
2024-04-16 00:55:11 +0800

    最新版本

--- Machine-Translation/Transformer/Visualize/visualize.py
+++ Machine-Translation/Transformer/Visualize/visualize.py
@@ -35,6 +35,7 @@ from torchtext.data import get_tokenizer
 # 设置忽略warning信息
 warnings.filterwarnings("ignore")
 # 设置绘图参数
+sns.set(font_scale=1.25)
 plt.rcParams["font.family"] = ["Microsoft YaHei"]  # 使用微软雅黑字体
 
 # 定义特殊TOKENdd
@@ -60,15 +61,16 @@ n_heads = 8
 d_model = 512
 d_ff = 2048
 dropout = 0.1
-max_len = 40
-resume = "../Tab-Separator/ckpt/en-zh/e6d6h8dm64df2048ml40/best_model.pth"
+max_len = 5000
+resume = "../Tab-Separator/ckpt/en-zh/e6d6h8dm512df2048ml5000/best_model.pth"
 
 # 数据参数
 data_dir = "../Tab-Separator/data/eng-zh.txt"
-src_dict = "../Tab-Separator/ckpt/en-zh/e6d6h8dm64df2048ml40/words/src_dict.txt"
-tgt_dict = "../Tab-Separator/ckpt/en-zh/e6d6h8dm64df2048ml40/words/tgt_dict.txt"
+src_dict = "../Tab-Separator/ckpt/en-zh/e6d6h8dm512df2048ml5000/words/src_dict.txt"
+tgt_dict = "../Tab-Separator/ckpt/en-zh/e6d6h8dm512df2048ml5000/words/tgt_dict.txt"
 
-cmap = sns.cubehelix_palette(start=1.5, rot=3, gamma=0.8, as_cmap=True)
+# cmap = sns.cubehelix_palette(start=1.5, rot=3, gamma=0.8, as_cmap=True)
+cmap = "RdBu_r"
 
 # 设置训练硬件
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -85,11 +87,12 @@ output_dir.mkdir(exist_ok=True)
 def heatmap(
     data: Optional[List],
     titles: List[str] = None,
-    x_ticks: Tuple[List[List[str]], int] = None,
-    y_ticks: Tuple[List[List[str]], int] = None,
+    x_ticks: List[List[str]] = "auto",
+    y_ticks: List[List[str]] = "auto",
+    square: bool = False,
     n_rows: int = 1,
     n_cols: int = 1,
-    fig_size: Tuple[int, int] = (8, 8),
+    fig_size: Tuple[int, int] = (15, 10),
     cbar: bool = True,
     annot: bool = False,
     fmt: str = ".0f",
@@ -104,18 +107,21 @@ def heatmap(
         else:
             ax = axes[i // n_cols, i % n_cols]
 
-        sns.heatmap(data[i], cbar=cbar, annot=annot, fmt=fmt, ax=ax, cmap=cmap)
+        sns.heatmap(
+            data[i],
+            cbar=cbar,
+            annot=annot,
+            fmt=fmt,
+            ax=ax,
+            square=square,
+            cmap=cmap,
+            annot_kws={"size": 10},
+            xticklabels=x_ticks[i],
+            yticklabels=y_ticks[i],
+        )
 
         if titles:
             ax.set_title(titles[i])
-        if x_ticks:
-            x_tick, rotation = x_ticks
-            ticks = np.linspace(0.5, len(x_tick[i]) - 0.5, len(x_tick[i]))
-            ax.set_xticks(ticks, rotation=rotation, labels=x_tick[i])
-        if y_ticks:
-            y_tick, rotation = y_ticks
-            ticks = np.linspace(0.5, len(y_tick[i]) - 0.5, len(y_tick[i]))
-            ax.set_yticks(ticks, rotation=rotation, labels=y_tick[i])
 
     return fig, axes
 
@@ -399,9 +405,6 @@ model = Transformer(
 
 if resume:
     resume = torch.load(resume)
-    resume["model"]["positional_encoding.pos_embedding"] = resume["model"]["positional_encoding.pos_embedding"][
-        :max_len, :, :
-    ]
     model.load_state_dict(resume["model"])
 
 
@@ -411,10 +414,10 @@ if resume:
 
 fig_src, ax_src = heatmap(
     [src[:, i : i + 1].cpu() for i in range(len(data_idx))],
-    x_ticks=([[str(i)] for i in range(5)], 0),
-    y_ticks=([sent[0] for sent in sentences], 0),
+    x_ticks=[[str(i)] for i in range(5)],
+    y_ticks=[sent[0] for sent in sentences],
     n_cols=5,
-    fig_size=(8, 8),
+    fig_size=(10, 10),
     cbar=False,
     annot=True,
     fmt="d",
@@ -426,8 +429,8 @@ fig_src.savefig(output_dir / "data_src_token.png")
 
 fig_tgt, ax_tgt = heatmap(
     [tgt[:, i : i + 1].cpu() for i in range(len(data_idx))],
-    x_ticks=([[str(i)] for i in range(5)], 0),
-    y_ticks=([sent[1] for sent in sentences], 0),
+    x_ticks=[[str(i)] for i in range(5)],
+    y_ticks=[sent[1] for sent in sentences],
     n_cols=5,
     fig_size=(8, 8),
     cbar=False,
@@ -445,8 +448,10 @@ fig_tgt.savefig(output_dir / "data_tgt_token.png")
 ++++++++++++++++++++++++++++++"""
 
 fig_pe, ax_pe = heatmap(
-    [model.positional_encoding.pos_embedding.reshape(-1, d_model).cpu()],
-    fig_size=(28, 10),
+    [model.positional_encoding.pos_embedding.cpu()[:40, 0, :64]],
+    fig_size=(32, 10),
+    x_ticks=[True],
+    y_ticks=[True],
     cbar=True,
     annot=True,
     fmt=".2f",
@@ -465,8 +470,8 @@ src_emb = model.src_tok_emb(src)
 tgt_emb = model.tgt_tok_emb(tgt_input)
 
 fig_src_emb, ax_src_emb = heatmap(
-    [src_emb.cpu().detach()[:, 0, :].reshape(-1, d_model)],
-    y_ticks=([sentences[0][0]], 0),
+    [src_emb.cpu().detach()[:, 0, :64]],
+    y_ticks=[sentences[0][0]],
     fig_size=(36, 10),
     cbar=True,
     annot=True,
@@ -486,8 +491,8 @@ src_pos_emb = model.positional_encoding(src_emb)
 tgt_pos_emb = model.positional_encoding(tgt_emb)
 
 fig_src_pos_emb, ax_src_pos_emb = heatmap(
-    [src_pos_emb[:, 0, :].cpu().detach()],
-    y_ticks=([sentences[0][0]], 0),
+    [src_pos_emb[:, 0, :64].cpu().detach()],
+    y_ticks=[sentences[0][0]],
     fig_size=(36, 10),
     cbar=True,
     annot=True,
@@ -499,54 +504,46 @@ fig_src_pos_emb.tight_layout()
 fig_src_pos_emb.savefig(output_dir / "emb_src_pos.png")
 
 """+++++++++++++++++++++++++++++++++++++++++++++++
-@@@ encoder 第一层 Self-Attention 推理
+@@@ encoder 推理
 +++++++++++++++++++++++++++++++++++++++++++++++"""
 
-# 计算每个头的维度
-src_len, bsz, embed_dim = src_pos_emb.shape
-tgt_len, _, _ = tgt_pos_emb.shape
+attn_mask = src_mask
+key_padding_mask = src_padding_mask
+last_attn = model.transformer.encoder.layers[-1].self_attn
+
+out = src_pos_emb
+for mod in model.transformer.encoder.layers[:-1]:
+    out = mod(out, src_mask=src_mask, src_key_padding_mask=src_padding_mask)
+
+query, key, value = out, out, out
+
+# set sup shape vars
+tgt_len, bsz, embed_dim = query.shape
+src_len, _, _ = key.shape
+
 head_dim = embed_dim // n_heads
 
-# 获取 transformer.encoder 第一层 Self Multi-head Attention 权重信息
-input_proj_weight = model.transformer.encoder.layers[0].self_attn.in_proj_weight  # q, k, v 映射的 weight
-input_proj_bias = model.transformer.encoder.layers[0].self_attn.in_proj_bias  # q, k, v 映射的 bias
-out_proj_weight = model.transformer.encoder.layers[0].self_attn.out_proj.weight  # concat 部分的 weight
-out_proj_bias = model.transformer.encoder.layers[0].self_attn.out_proj.bias  # concat 部分的 bias
+#
+# compute in-projection
+#
+q, k, v = F._in_projection_packed(query, key, value, last_attn.in_proj_weight, last_attn.in_proj_bias)
 
-# 以下推理片段节选自 torch.nn.functional._in_projection_packed
-# 将输入矩阵利用线性映射到低维的多头 q, k, v
-q, k, v = F.linear(src_pos_emb, input_proj_weight, input_proj_bias).chunk(3, dim=-1)
-# 将 q, k, v 利用线性映射分成多个头，形状为 (batch * num_heads, src_len, embed_dim // num_heads)
-q = q.contiguous().view(src_len, bsz * n_heads, head_dim).transpose(0, 1)
+# prep attention mask
+attn_mask = attn_mask.unsqueeze(0)
+
+#
+# reshape q, k, v for multihead attention and make em batch first
+#
+q = q.contiguous().view(tgt_len, bsz * n_heads, head_dim).transpose(0, 1)
 k = k.contiguous().view(k.shape[0], bsz * n_heads, head_dim).transpose(0, 1)
 v = v.contiguous().view(v.shape[0], bsz * n_heads, head_dim).transpose(0, 1)
 
-# 多头 q, k, v 可视化
-q_vis = q.view(bsz, n_heads, src_len, head_dim)
-fig_smha_q, ax_smha_q = heatmap(
-    [q_vis[0, i, :, :8].cpu().detach() for i in range(n_heads)],
-    y_ticks=([sentences[0][0] for _ in range(n_heads)], 0),
-    n_rows=2,
-    n_cols=4,
-    titles=["head-%d" % (i + 1) for i in range(n_heads)],
-    fig_size=(20, 12),
-    cbar=True,
-    annot=True,
-    fmt=".2f",
-    cmap=cmap,
-)
-fig_smha_q.suptitle("Self Multi-Head Attention Query")
-fig_smha_q.tight_layout()
-fig_smha_q.savefig(output_dir / "smha_query.png")
-
-"""+++++++++++++++++++++++++++
-@@@ Self-Attention mask 可视化
-+++++++++++++++++++++++++++"""
+# update source sequence length after adjustments
+src_len = k.size(1)
 
 # merge key padding and attention masks
-attn_mask = src_mask.unsqueeze(0)
 key_padding_mask = (
-    src_padding_mask.view(bsz, 1, 1, src_len).expand(-1, n_heads, -1, -1).reshape(bsz * n_heads, 1, src_len)
+    key_padding_mask.view(bsz, 1, 1, src_len).expand(-1, n_heads, -1, -1).reshape(bsz * n_heads, 1, src_len)
 )
 attn_mask = attn_mask.logical_or(key_padding_mask)
 
@@ -555,98 +552,53 @@ new_attn_mask = torch.zeros_like(attn_mask, dtype=q.dtype)
 new_attn_mask.masked_fill_(attn_mask, float("-inf"))
 attn_mask = new_attn_mask
 
-attn_mask_vis = attn_mask.view(bsz, n_heads, src_len, src_len)
-fig_smha_mask, ax_smha_mask = heatmap(
-    [attn_mask_vis[i, 0].cpu() for i in range(len(data_idx))],
-    x_ticks=([sent[0] for sent in sentences], 90),
-    y_ticks=([sent[0] for sent in sentences], 0),
-    n_cols=5,
-    fig_size=(40, 10),
-    cbar=False,
-    annot=True,
-    fmt=".0f",
-    cmap=cmap,
-)
-fig_smha_mask.suptitle("Self-Attention Mask")
-fig_smha_mask.tight_layout()
-fig_smha_mask.savefig(output_dir / "smha_mask.png")
+#
+# (deep breath) calculate attention and out projection
+#
+attn_output, attn_output_weights = F._scaled_dot_product_attention(q, k, v, attn_mask, 0.0)
+attn_output = attn_output.transpose(0, 1).contiguous().view(tgt_len * bsz, embed_dim)
+attn_output = F.linear(attn_output, last_attn.out_proj.weight, last_attn.out_proj.bias)
+attn_output = attn_output.view(tgt_len, bsz, attn_output.size(1))
 
-"""++++++++++++++++++
-@@@ 点乘注意力计算
-++++++++++++++++++"""
+attn_output_weights = attn_output_weights.view(bsz, n_heads, tgt_len, src_len)
 
-# Softmax(Q·K)/sqrt(d_model//n_heads)
-# 采用了 torch.baddbmm 和 torch.bmm 的方式，将 Multi-Head 和 batch 合在一起的方式进行矩阵乘法
-attn_output, attn_output_weights = scaled_dot_product_attention(q, k, v, attn_mask)
-attn_output_weights = attn_output_weights.view(bsz, n_heads, src_len, src_len)
 
-"""+++++++++++++++++++++++++++++++++++++++++++++++
-@@@ 对 Self-Attention 多头注意力权重 可视化
-+++++++++++++++++++++++++++++++++++++++++++++++"""
+# -------------------------------- #
+# Multihead Attention Head Weight
+# -------------------------------- #
 
-fig_smha_head_weights, ax5_attn_head_weights = heatmap(
-    [attn_output_weights[0, i, :, :].cpu().detach() for i in range(n_heads)],
-    x_ticks=([sentences[0][0] for _ in range(n_heads)], 90),
-    y_ticks=([sentences[0][0] for _ in range(n_heads)], 0),
-    n_rows=2,
-    n_cols=4,
-    titles=["head-%d" % (i + 1) for i in range(n_heads)],
-    fig_size=(20, 12),
-    cbar=True,
-    annot=False,
-    fmt=".2f",
-    cmap=cmap,
-)
-fig_smha_head_weights.suptitle("Self Multi-head Attention Weights")
-fig_smha_head_weights.tight_layout()
-fig_smha_head_weights.savefig(output_dir / "smha_head_weights.png")
+for i in range(n_heads):
+    fig_smha_head_weight, ax_smha_head_weight = heatmap(
+        [attn_output_weights[0, i].cpu().detach()],
+        x_ticks=[sentences[0][0]],
+        y_ticks=[sentences[0][0]],
+        fig_size=(20, 10),
+        cmap=cmap,
+        annot=True,
+        fmt=".2f",
+    )
+    fig_smha_head_weight.suptitle("Head-%d" % i)
+    plt.show()
 
-"""+++++++++++++++++++++++++++++++++++++++++++++++
-@@@ Self-Attention 多头平均注意力权重 可视化
-+++++++++++++++++++++++++++++++++++++++++++++++"""
+# ------------------------------------ #
+# Multihead Attention Weight (Average)
+# ------------------------------------ #
 
-fig_smha_weight, ax_smha_weight = heatmap(
-    (attn_output_weights.sum(dim=1) / n_heads).cpu().detach(),
-    x_ticks=([sent[0] for sent in sentences], 90),
-    y_ticks=([sent[0] for sent in sentences], 0),
-    n_cols=5,
-    fig_size=(80, 10),
-    cbar=True,
-    annot=True,
-    fmt=".2f",
-    cmap=cmap,
-)
-fig_smha_weight.suptitle("Self Multi-head Attention Weight (Average)")
-fig_smha_weight.tight_layout()
-fig_smha_weight.savefig(output_dir / "smha_weight.png")
+attn_output_weights = attn_output_weights.sum(dim=1) / n_heads
+for i in range(len(data_idx)):
+    fig_smha_head_weight, ax_smha_head_weight = heatmap(
+        [attn_output_weights[i].cpu().detach()],
+        x_ticks=[sentences[i][0]],
+        y_ticks=[sentences[i][0]],
+        fig_size=(20, 10),
+        cmap=cmap,
+        annot=True,
+        fmt=".2f",
+    )
+    fig_smha_head_weight.suptitle("Self-Attention Weight %d" % (i + 1))
+    fig_smha_head_weight.tight_layout()
+    fig_smha_head_weight.savefig(output_dir / "smha_weight")
 
-"""+++++++++++++++++++++++++++++++++++++++++++++++
-@@@ Self-Attention 推理结果 可视化
-+++++++++++++++++++++++++++++++++++++++++++++++"""
-
-# concat 后映射到 d_model
-attn_output = attn_output.transpose(0, 1).contiguous().view(src_len * bsz, embed_dim)
-attn_output = F.linear(attn_output, out_proj_weight, out_proj_bias)
-attn_output = attn_output.view(src_len, bsz, attn_output.size(1))
-
-fig_smha_output, ax_smha_output = heatmap(
-    [attn_output[:, 0, :].cpu().detach().reshape(-1, d_model)[:, :]],
-    y_ticks=([sentences[0][0]], 0),
-    fig_size=(32, 10),
-    cbar=True,
-    annot=True,
-    fmt=".2f",
-    cmap=cmap,
-)
-fig_smha_output.suptitle("Self Multi-head Attention Output")
-fig_smha_output.tight_layout()
-fig_smha_output.savefig(output_dir / "smha_output.png")
-
-"""+++++++++++++++++++++++++++++++++++++++++++++++
-@@@ 整个 Encoder 推理
-+++++++++++++++++++++++++++++++++++++++++++++++"""
-
-memory = model.transformer.encoder(src_pos_emb, src_mask, src_padding_mask)
 
 """+++++++++++++++++++++++++++++++++++++++++++++++
 @@@ Decoder 第一层推理
